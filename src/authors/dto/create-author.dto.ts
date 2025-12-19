646714f94511b4f81a2b6eae62d51fe2f8cd2d1e
Carlo Allan Laynes <carloallanlaynes@gmail.com>
2025-12-20 01:48:23 +0800

    feat: add validated DTOs for authors and books

--- src/authors/dto/create-author.dto.ts
+++ src/authors/dto/create-author.dto.ts
@@ -1,1 +1,23 @@
-export class CreateAuthorDto {}
+import {
+  IsOptional,
+  IsString,
+  IsDateString,
+  IsNotEmpty,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateAuthorDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
+  bio?: string;
+
+  @IsOptional()
+  @IsDateString()
+  birthDate?: string;
+}
